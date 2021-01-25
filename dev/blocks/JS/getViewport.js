da086ab6edb027101785bf4bc61dbec2741e6c08
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-01-25 20:02:07 +0100

    feat: create variable to get viewport width & height

---
+++ dev/blocks/JS/getViewport.js
@@ -0,0 +1,8 @@
+export const vw = Math.max(
+  document.documentElement.clientWidth || 0,
+  window.innerWidth || 0
+);
+export const vh = Math.max(
+  document.documentElement.clientHeight || 0,
+  window.innerHeight || 0
+);

74beb7b3d900587351e5b54c8fb2d552ea42d64a
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-01-26 13:47:25 +0100

    fix: only execute code if the element exists

--- dev/script.js
+++ dev/script.js
@@ -7,69 +7,74 @@ import { fontSettingsAction } from "./blocks/JS/fontSettings/fontSettingsAction"
 import { fontSettingsContainer } from "./blocks/JS/fontSettings/fontSettingsContainer";
 import { checkForStorage } from "./blocks/JS/fontSettings/changeFontSettings";
 
-checkForStorage();
-
 /**
- * Listen if User tapped outside the Font Settings
- *
- * @return      Remove class .show-flex from #font-settings
+ * If chapter exists, execute the following codes
  */
+if (chapter !== null) {
+  checkForStorage();
 
-let boolFontSettings = false;
+  /**
+   * Listen if User tapped outside the Font Settings
+   *
+   * @return      Remove class .show-flex from #font-settings
+   */
 
-onDoubleTap(chapter, (e) => {
-  fontSettingsContainer.classList.add("show-flex");
-  boolFontSettings = true;
-});
+  let boolFontSettings = false;
 
-window.addEventListener("touchstart", (e) => {
-  if (boolFontSettings) {
-    const boolInside = e.path.includes(fontSettingsContainer);
-    if (boolInside) {
-      // do something
-    } else {
-      fontSettingsContainer.classList.remove("show-flex");
-    }
-  }
-});
+  onDoubleTap(chapter, (e) => {
+    fontSettingsContainer.classList.add("show-flex");
+    boolFontSettings = true;
+  });
 
-/**
- * Makes the .info-box into a collapseable box,
- * toggling between .show-block
- *
- * @param   {Array}           infoBox         Array of HtmlElements
- * @param   {HtmlElement}     box             Single Item of infoBox
- * @param   {Bool}            boolIsInfoBox   Checks if click event is inside infoBox
- */
-const infoBox = Array.from(document.querySelectorAll(".info-box"));
-
-chapter.addEventListener("click", (e) => {
-  infoBox.forEach((box) => {
-    const boolIsInfoBox = e.path.includes(box);
-    if (boolIsInfoBox) {
-      const infoText = box.querySelector(".info-text");
-      infoText.classList.toggle("show-block");
+  window.addEventListener("touchstart", (e) => {
+    if (boolFontSettings) {
+      const boolInside = e.path.includes(fontSettingsContainer);
+      if (boolInside) {
+        // do something
+      } else {
+        fontSettingsContainer.classList.remove("show-flex");
+      }
     }
   });
-});
 
-/**
- * Makes the Footnote collapseable
- *
- * @param   {HtmlElement}   footnote        Single Item of hasFootnote
- * @param   {Bool}          boolIsFootnote  Checks if clicked area is a footnote
- */
-const hasFootnote = Array.from(document.querySelectorAll(".has-footnote"));
+  /**
+   * Makes the .info-box into a collapseable box,
+   * toggling between .show-block
+   *
+   * @param   {Array}           infoBox         Array of HtmlElements
+   * @param   {HtmlElement}     box             Single Item of infoBox
+   * @param   {Bool}            boolIsInfoBox   Checks if click event is inside infoBox
+   */
+  const infoBox = Array.from(document.querySelectorAll(".info-box"));
 
-chapter.addEventListener("click", (e) => {
-  hasFootnote.forEach((footnote) => {
-    const boolIsFootnote = e.path.includes(footnote);
-
-    if (boolIsFootnote) {
-      const footnoteNote = footnote.querySelector(".footnote-note");
-      footnoteNote.classList.toggle("show-block");
-    }
+  chapter.addEventListener("click", (e) => {
+    infoBox.forEach((box) => {
+      const boolIsInfoBox = e.path.includes(box);
+      if (boolIsInfoBox) {
+        const infoText = box.querySelector(".info-text");
+        infoText.classList.toggle("show-block");
+      }
+    });
   });
-});
 
-fontSettingsAction();
+  /**
+   * Makes the Footnote collapseable
+   *
+   * @param   {HtmlElement}   footnote        Single Item of hasFootnote
+   * @param   {Bool}          boolIsFootnote  Checks if clicked area is a footnote
+   */
+  const hasFootnote = Array.from(document.querySelectorAll(".has-footnote"));
+
+  chapter.addEventListener("click", (e) => {
+    hasFootnote.forEach((footnote) => {
+      const boolIsFootnote = e.path.includes(footnote);
+
+      if (boolIsFootnote) {
+        const footnoteNote = footnote.querySelector(".footnote-note");
+        footnoteNote.classList.toggle("show-block");
+      }
+    });
+  });
+
+  fontSettingsAction();
+}

97a2f881d73d2862450e04a00107f235b17bdb9a
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-02-09 14:39:47 +0100

    dev: create started file for homepage

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Theka</title>
+      </Head>
+      <section>
+        <h1>Hallo! :)</h1>
+      </section>
+    </>
+  );
+}
